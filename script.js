10a34ada7046b6277329b2697406ca735609503b
Kaio <kaiocesa2009@gmail.com>
2023-09-26 14:16:06 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle('light')
+
+
+  const img = querySelector
+
+
+  if(html.classList.contains('light')) {
+
+    img.setAttribute('src,', './assets/avatar-light.png')
+  } else {
+
+    img.setAttribute('src', './assets/avatar.png')
+  }
+}
